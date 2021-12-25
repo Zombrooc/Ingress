d8116047ec8eb2146ddbb000660b064fd709bc7f
Zombroc <elian.valdez09@gmail.com>
2021-12-25 15:47:59 +0000

    eslint-disable-next-line no-unused-vars

--- src/v1/Middlewares/errorHandlerMiddleware.js
+++ src/v1/Middlewares/errorHandlerMiddleware.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
